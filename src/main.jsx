582a5de054cca0e595cd868d16ebb47bb1736cbd
TechnoZombie <124879219+TechnoZombie@users.noreply.github.com>
2025-04-01 23:11:40 +0100

    revert ro browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <HashRouter>
+    <BrowserRouter basename="/showroom">
         <App/>
-    </HashRouter>
+    </BrowserRouter>
 );
