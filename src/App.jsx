1365b69108c4c91cdec6be193d1034dbdead14b9
TechnoZombie <124879219+TechnoZombie@users.noreply.github.com>
2025-06-13 23:39:30 +0100

    fix to scroll issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import './css/Header.css';
 
 import Sidebar from "./components/Sidebar.jsx";
 import Showcase from "./components/Showcase.jsx";
+import ScrollToTop from "./utils/ScrollToTop.jsx";
 
 function App() {
     return (
         <>
+            <ScrollToTop />
             <Sidebar/>
             <Showcase/>
         </>
