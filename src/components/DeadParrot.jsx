ce970eaf472831f9cbd777880df5e3d95870c0c5
TechnoZombie <124879219+TechnoZombie@users.noreply.github.com>
2026-05-10 17:14:27 +0100

    added galery and images to DeadParrot;

--- src/components/DeadParrot.jsx
+++ src/components/DeadParrot.jsx
@@ -3,6 +3,7 @@ import {motion} from "framer-motion";
 import GitHubButtonAndTechTag from "../utils/GitHubButtonAndTechTag.jsx";
 import MarkdownRenderer from "../utils/MarkdownRenderer.jsx";
 import deadParrotMarkdown from "./markdownFiles/deadParrot-markdown.js";
+import ImageGallery from "../utils/ImageGallery.jsx";
 
 const link = "https://github.com/technozombie/deadparrot";
 
@@ -18,6 +19,13 @@ const DeadParrot = () => {
                 link={link}
                 techs={["java"]}/>
 
+            <ImageGallery
+                images={[
+                    {src: "./dp/dp01.png", alt: "DeadParrot Listening"},
+                    {src: "./dp/dp02.png", alt: "DeadParrot Played back"},
+                    {src: "./dp/dp03.png", alt: "DeadParrot Audio probe"}
+                ]}
+            />
             <MarkdownRenderer markdownContent={deadParrotMarkdown}/>
 
         </motion.div>);
